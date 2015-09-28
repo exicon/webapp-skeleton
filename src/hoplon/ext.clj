d1c2c54c1b5b76afe3c498d8dbf3e3c415bec3fe
Tamas Herman <tamas@exiconglobal.com>
2015-09-28 15:22:08 +0800

    Simpler `loop-tpl` called `x`
    it also wraps the collection to loop over into a formula cell,
    because it's often forgotten and leads to a non-descriptive:
    "Uncaught Error: [object Object] is not ISeqable" error.

--- src/hoplon/ext.clj
+++ src/hoplon/ext.clj
@@ -1,3 +1,10 @@
-(ns hoplon.ext)
+(ns hoplon.ext
+  (:require
+    [hoplon.core :as h]
+    [javelin.core :as j]))
 
 (defmacro macro-test [] 123)
+
+(defmacro x [[bindings items] body]
+  `(h/loop-tpl* (j/cell= ~items)
+                (fn [item#] (j/cell-let [~bindings item#] ~body))))
